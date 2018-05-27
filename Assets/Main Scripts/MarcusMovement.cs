134bc38cea1dfbd47b878831d6fbac2a18cb8031
Jakob Leech <rockyman218@gmail.com>
2018-05-28 08:05:03 +1200

    ch

---
+++ Assets/Main Scripts/MarcusMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarcusMovement : MonoBehaviour {
+
+    [HideInInspector]
+    public bool CanMove = true;
+
+    public Vector3 MarcusPosition = new Vector3(0, 0, 0);
+    public float MarcusSpeed = 40;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = MarcusPosition;
+
+        float HorizontalInput = Input.GetAxis("Horizontal");
+
+        if (CanMove)
+        {
+            MarcusPosition.x += HorizontalInput * (MarcusSpeed / 100);
+        }
+	}
+}
